f268b3793058b653e9ed1e5326b85e8fffd3f97e
vimalathithan <vimalathithanit@gmail.com>
2016-02-20 00:42:47 -0800

    Basic twitter implementation

--- app/src/main/java/com/codepath/tweets/tweets/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/tweets/tweets/TweetsArrayAdapter.java
@@ -1,9 +1,15 @@
 package com.codepath.tweets.tweets;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.tweets.tweets.models.Tweet;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -12,6 +18,28 @@ import java.util.List;
  */
 public class TweetsArrayAdapter extends ArrayAdapter<Tweet> {
     public TweetsArrayAdapter(Context context, List<Tweet> tweets) {
-        super(context, android.R.layout.simple_list_item_1, tweets);
+        super(context, 0, tweets);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //1. Get the tweet
+        Tweet tweet = getItem(position);
+        //2. Find or inflate the template
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
+        }
+        //3. Find the subviews to fill the data in the template
+        ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
+        TextView tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
+        TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
+        //4. Populate data into the subviews
+        tvUserName.setText(tweet.getUser().getScreenName());
+        tvBody.setText(tweet.getBody());
+        ivProfileImage.setImageResource(android.R.color.transparent);
+        Picasso.with(getContext()).load(tweet.getUser().getProfileImageUrl()).into(ivProfileImage);
+        //5. return the view to be onserted into the list
+        return convertView;
     }
 }
